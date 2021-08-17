261fa0b09b954697148aba4a6d15e7377bbd2258
onezeun <78632052+onezeun@users.noreply.github.com>
2021-08-18 00:18:21 +0900

    0817 header

---
+++ src/components/Header/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class HeaderContainer extends Component {
+    render() {
+        return (
+            <Header>
+                made by. 201호 개발자들
+            </Header>
+        );
+    }
+}
+
+export default HeaderContainer;
