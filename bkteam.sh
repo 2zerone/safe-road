7af8966afa69a1d24e5c128ea03e9638bbb2da22
zzambbang <zzambbang@gmail.com>
2021-08-17 23:15:08 +0900

    .

--- bkteam.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-cd ~/GitProjects/safe-road/projects/bkteam
-source ~/GitProjects/safe-road/venvs/bkteam/bin/activate
--- projects/bkteam/safe_road/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SafeRoadConfig(AppConfig):
-    name = 'safe_road'
